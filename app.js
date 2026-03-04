d5c8f6229cbd9b7ef376ed206e41bd6caa6252e9
Mode-s <shota.umemoto7@gmail.com>
2026-03-04 13:28:16 +0900

    feat: JavaScriptの変数宣言(let)と再代入の学習

--- app.js
+++ app.js
@@ -1,2 +1,5 @@
-const favoriteLanguage = "JavaScript";
-console.log(favoriteLanguage);
+let currentStatus = "学習中";
+console.log(currentStatus);
+
+currentStatus = "休憩中";
+console.log(currentStatus);
